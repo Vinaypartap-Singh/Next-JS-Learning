0d23a3a96353ea46a52cbe5ef402711b5167cca3
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-07-29 22:59:27 +0530

    Custom Api in Next JS App/Api Folder

---
+++ app/api/hello/route.js
@@ -0,0 +1,3 @@
+export async function GET(req) {
+  return new Response("Hello , First Api");
+}
---
+++ app/api/users/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  return NextResponse.json({
+    name: "Vinay Developer",
+    age: 17,
+    passion: "Web Development",
+    aim: "Perform Best In New Company",
+  });
+}
