22bdc75a83f25ef839aa06e4fd5ba1a558425cc7
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-07-29 23:00:59 +0530

    Status Code In Api and always name api js file as route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  return NextResponse.json({
-    name: "Vinay Developer",
-    age: 17,
-    passion: "Web Development",
-    aim: "Perform Best In New Company",
-  });
+  return NextResponse.json(
+    {
+      name: "Vinay Developer",
+      age: 17,
+      passion: "Web Development",
+      aim: "Perform Best In New Company",
+    },
+    { status: 200 }
+  );
 }
